c69ee9f42fce3f1a6c1cec9337e64fc154c73024
Eloy Bonilla <eloyjosebonillag@gmail.com>
2023-01-20 17:09:46 -0400

    New: Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { FaBeer } from 'react-icons/fa';
+import { GiAlliedStar,GiAbbotMeeple } from "react-icons/gi";
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <FaBeer/> Hola mundo <GiAlliedStar/><GiAbbotMeeple/>
+    </div>
+  )
+}
+
+export default App
